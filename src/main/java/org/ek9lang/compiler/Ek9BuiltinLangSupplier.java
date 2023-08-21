e9330a65f0c7150bcb8950c6910cb58a4fa39274
Steve Limb <stephenjohnlimb@gmail.com>
2023-08-21 12:20:31 +0100

    Updated html documentation and ek9 dynamic functions and classes examples.

--- src/main/java/org/ek9lang/compiler/Ek9BuiltinLangSupplier.java
+++ src/main/java/org/ek9lang/compiler/Ek9BuiltinLangSupplier.java
@@ -22,7 +22,7 @@ public class Ek9BuiltinLangSupplier implements Supplier<List<CompilableSource>> 
   /**
    * As we add more, update this.
    */
-  public static final int NUMBER_OF_EK9_SYMBOLS = 74;
+  public static final int NUMBER_OF_EK9_SYMBOLS = 75;
 
   //Obviously with ek9 the indentation is important.
   @SuppressWarnings({"Indentation"})
@@ -517,6 +517,13 @@ public class Ek9BuiltinLangSupplier implements Supplier<List<CompilableSource>> 
             List()
               -> arg0 as T
 
+            operator + as pure
+              -> arg as T
+              <- rtn as List of T?
+
+            operator +=
+              -> arg as T              
+              
           Optional of type T
             Optional()
             
