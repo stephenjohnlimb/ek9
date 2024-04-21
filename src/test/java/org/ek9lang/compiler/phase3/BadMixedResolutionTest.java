92daa241952abf63f97019ad139a7b736b45b5e5
Steve Limb <stephenjohnlimb@gmail.com>
2024-04-21 11:41:31 +0100

    Sorted out workarea to separate mixed resolution test,

---
+++ src/test/java/org/ek9lang/compiler/phase3/BadMixedResolutionTest.java
@@ -0,0 +1,30 @@
+package org.ek9lang.compiler.phase3;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.List;
+import org.ek9lang.compiler.CompilableProgram;
+import org.ek9lang.compiler.CompilationPhase;
+import org.ek9lang.compiler.common.PhasesTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Just tests bad method/property resolution.
+ */
+class BadMixedResolutionTest extends PhasesTest {
+
+  public BadMixedResolutionTest() {
+    super("/examples/parseButFailCompile/badMixedResolution",
+        List.of("bad.mixed.resolution"));
+  }
+
+  @Test
+  void testPhaseDevelopment() {
+    testToPhase(CompilationPhase.FULL_RESOLUTION);
+  }
+
+  @Override
+  protected void assertFinalResults(boolean compilationResult, int numberOfErrors, CompilableProgram program) {
+    assertFalse(compilationResult);
+  }
+}
