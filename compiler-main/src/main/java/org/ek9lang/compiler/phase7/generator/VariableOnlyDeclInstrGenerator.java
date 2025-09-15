37137cba94121a0d2379d91257fab22e95d1628b
Steve Limb <stephenjohnlimb@gmail.com>
2025-09-15 18:06:57 +0100

    refactored variable only declaration generator.

--- compiler-main/src/main/java/org/ek9lang/compiler/phase7/generator/VariableOnlyDeclInstrGenerator.java
+++ compiler-main/src/main/java/org/ek9lang/compiler/phase7/generator/VariableOnlyDeclInstrGenerator.java
@@ -2,13 +2,9 @@ package org.ek9lang.compiler.phase7.generator;
 
 import java.util.List;
 import java.util.function.Function;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.ek9lang.antlr.EK9Parser;
-import org.ek9lang.compiler.common.TypeNameOrException;
 import org.ek9lang.compiler.ir.instructions.IRInstr;
-import org.ek9lang.compiler.ir.instructions.MemoryInstr;
 import org.ek9lang.compiler.phase7.generation.IRGenerationContext;
-import org.ek9lang.compiler.phase7.support.VariableNameForIR;
 import org.ek9lang.core.AssertValue;
 
 /**
@@ -18,11 +14,8 @@ import org.ek9lang.core.AssertValue;
  * STACK-BASED: Now uses IRGenerationContext for scope management instead of parameter threading.
  * </p>
  */
-public final class VariableOnlyDeclInstrGenerator extends AbstractGenerator
+public final class VariableOnlyDeclInstrGenerator extends AbstractVariableDeclGenerator
     implements Function<EK9Parser.VariableOnlyDeclarationContext, List<IRInstr>> {
-
-  private final TypeNameOrException typeNameOrException = new TypeNameOrException();
-  private final VariableNameForIR variableNameForIR = new VariableNameForIR();
 
   public VariableOnlyDeclInstrGenerator(final IRGenerationContext stackContext) {
     super(stackContext);
@@ -40,21 +33,5 @@ public final class VariableOnlyDeclInstrGenerator extends AbstractGenerator
     AssertValue.checkNotNull("VariableOnlyDeclarationContext cannot be null", ctx);
 
     return getDeclInstrs(ctx);
-  }
-
-  private List<IRInstr> getDeclInstrs(final ParseTree ctx) {
-    AssertValue.checkNotNull("Ctx cannot be null", ctx);
-
-    // Get symbol from the parsed module
-    final var variableSymbol = getRecordedSymbolOrException(ctx);
-
-    final var variableName = variableNameForIR.apply(variableSymbol);
-    final var variableTypeName = typeNameOrException.apply(variableSymbol);
-
-    // STACK-BASED: Create debug info using stack context
-    final var debugInfo = stackContext.createDebugInfo(variableSymbol.getSourceToken());
-
-    // Create memory reference instruction with proper debug info
-    return List.of(MemoryInstr.reference(variableName, variableTypeName, debugInfo));
   }
 }
